ce1680adcba4a47f5cf2c9f6f70480c8d5594495
Takayuki Okuda <momori.nyan@gmail.com>
2021-07-02 14:40:01 +0900

    add binary tree

--- simple-cc.c
+++ simple-cc.c
@@ -101,7 +101,7 @@ Token *tokenize(){
             continue;
         }
 
-        if (*p == '+' || *p == '-'){
+        if (strchr("+-*/()", *p)){
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -111,39 +111,141 @@ Token *tokenize(){
             cur->val = strtol(p, &p, 10);
             continue;
         }
-        error_at(p, "トークナイズできません");
+        error_at(p, "invalid token");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
 }
 
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // integer
+} NodeKind;
+
+typedef struct node {
+    NodeKind kind; // type of node
+    struct node *lhs; // left
+    struct node *rhs; // right
+    int val;
+} Node;
+
+Node *new_node(NodeKind kind){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_num(int val){
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_binary(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_binary(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_binary(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_binary(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *primary() {
+    // if next token is "(" , it must be "(" expr ")".
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // else it must be integer.
+    return new_num(expect_number());
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2){
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+        error("%s: invalid number of argmunts", argv[0]);
     }
 
     // tokenize
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
+    // output asemmbly
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    printf("    mov rax, %d\n", expect_number());
+    // generate code using tree
+    gen(node);
 
-    while (!at_eof()) {
-        if (consume('+')){
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
+    // load stack top 
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
